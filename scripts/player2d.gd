035a457b2f4380743efa61befb632f563b08ad0d
Jack <jack@Davids-Mac-mini.local>
2026-09-15 12:58:20 -0700

    now you freeze when you go into the ladder, but still nothing happends

--- scripts/player2d.gd
+++ scripts/player2d.gd
@@ -9,6 +9,7 @@ extends CharacterBody2D
 
 var target_position: Vector2 = Vector2.ZERO
 var is_moving: bool = false
+var can_move: bool = false
 
 
 func _ready() -> void:
@@ -16,21 +17,67 @@ func _ready() -> void:
 	var snapped_start_pos = map_layer.map_to_local(current_cell)
 	position = snapped_start_pos
 	target_position = snapped_start_pos
+	can_move = true
 
 
 func _physics_process(delta: float) -> void:
 	if is_moving:
-		position = position.lerp(target_position, speed * delta)
+		var behavior_speed = speed * 1.5
+		position += (target_position - position) * behavior_speed * delta
 		
-		if position.distance_to(target_position) < 1.0:
+		if position.distance_to(target_position) < 0.5:
 			position = target_position
 			is_moving = false
-		else:
-			position = position.lerp(target_position, speed * delta)
+			
+			var current_grid_pos = map_layer.local_to_map(position)
+			# freeze if you is going into a ladder
+			if cell_matches_id(current_grid_pos, "block_type", 2):
+				can_move = false
 	else:
-		get_input()
+		move()
+
+
+func move() -> void:
+	if can_move:	
+		var direction = get_input()
+		var anim_direction = direction
 		
-func get_input() -> void:
+		# animate
+		if anim_direction == Vector2.ZERO:
+			if anim.animation == "left":
+				anim.animation = "idle_left"
+			else:
+				if !anim.animation == "idle_left":
+					anim.animation = "idle_right"
+					
+		if anim_direction == Vector2.LEFT:
+			anim.animation = "left"
+		elif anim_direction == Vector2.RIGHT:
+			anim.animation = "right"
+
+		if direction != Vector2.ZERO:
+			var current_grid_pos = map_layer.local_to_map(position)
+			var next_grid_pos = current_grid_pos + Vector2i(direction)
+			var tile_data = cell_matches_id(next_grid_pos, "block_type", 1)
+		
+			if tile_data != null:
+				if tile_data == true:
+					print("movement blocked")
+				else:
+					tile_data = cell_matches_id(next_grid_pos, "block_type", 2)
+					if tile_data != null:
+						if tile_data == true:
+							ladder()
+						else:
+							target_position = map_layer.map_to_local(next_grid_pos)
+							is_moving = true
+					else:
+						target_position = map_layer.map_to_local(next_grid_pos)
+						is_moving = true
+			else:
+				print("No tile painted here! Cannot walk into empty grid space.")
+		
+func get_input():
 	var direction := Vector2.ZERO
 	
 	# get input
@@ -45,35 +92,9 @@ func get_input() -> void:
 			
 	elif Input.is_action_just_pressed("move_up"):
 		direction = Vector2.UP
-		
-	var anim_direction := direction
 	
-	# animate
-	if anim_direction == Vector2.ZERO:
-		if anim.animation == "left":
-			anim.animation = "idle_left"
-		else:
-			if !anim.animation == "idle_left":
-				anim.animation = "idle_right"
-				
-	if anim_direction == Vector2.LEFT:
-		anim.animation = "left"
-	elif anim_direction == Vector2.RIGHT:
-		anim.animation = "right"
-
-	if direction != Vector2.ZERO:
-		var current_grid_pos = map_layer.local_to_map(position)
-		var next_grid_pos = current_grid_pos + Vector2i(direction)
-		var tile_data = cell_matches_id(next_grid_pos, "block_type", 1)
+	return direction
 	
-		if tile_data != null:
-			if tile_data == true:
-				print("movement blocked")
-			else:
-				target_position = map_layer.map_to_local(next_grid_pos)
-				is_moving = true
-		else:
-			print("No tile painted here! Cannot walk into empty grid space.")
 
 func cell_matches_id(grid_pos: Vector2i, layer_name: String, target_id: int) -> bool:
 	var tile_data: TileData = map_layer.get_cell_tile_data(grid_pos)
@@ -84,7 +105,12 @@ func cell_matches_id(grid_pos: Vector2i, layer_name: String, target_id: int) -> 
 			return int(value) == target_id
 
 	return false
-
-
-	
 	var Position = position
+
+func ladder():
+	var direction = get_input()
+	print("at ladder")
+	var current_grid_pos = map_layer.local_to_map(position)
+	var next_grid_pos = current_grid_pos + Vector2i(direction)
+	target_position = map_layer.map_to_local(next_grid_pos)
+	is_moving = true
