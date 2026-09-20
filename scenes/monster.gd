93df88b830f0f479b310686b48f97465e0f817a9
Jack <jack@Davids-Mac-mini.local>
2026-09-20 13:41:02 -0700

    added navMesh, add AI next

--- scenes/monster.gd
+++ scenes/monster.gd
@@ -3,12 +3,11 @@ extends CharacterBody2D
 const Player2D = preload("res://scripts/player2d.gd")
 var player: Player2D = Player2D.new()
 var tile_size: float = player.tile_size
-
+var current_path: Array[Vector2] = []
+var speed = tile_size
+var direction = Vector2.RIGHT
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var tile_map: TileMapLayer = $"../TileMapLayer"
 
-var target_position: Vector2 = Vector2.ZERO
-var is_moving: bool = false
-var can_move: bool = false
-
-func _ready() -> void:
-	pass
+func ready() -> void:
+	anim.play("idle")
