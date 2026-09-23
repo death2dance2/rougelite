ff74939e0d02f5bed115e7d3adbaffb575e508f9
Jack <jack@Davids-Mac-mini.local>
2026-09-23 08:50:52 -0700

    i am saving the new movement sy stem, easy grid snap movement, no collision. may need to change the size of the grid

--- scripts/player2d.gd
+++ scripts/player2d.gd
@@ -1,5 +1,30 @@
 extends CharacterBody2D
 
-const tile_size = 16
+const tile_size = 10
 
-signal player_did_a_move
+var direction = Vector2.ZERO
+var speed = 30.0
+
+func _ready() -> void:
+	global_position = Vector2(0, 0)
+
+func _physics_process(delta: float) -> void:
+	direction = get_input()
+	
+	if direction != Vector2.ZERO:
+		global_position += direction * tile_size
+
+func get_input():
+	if Input.is_action_just_pressed("move_right"):
+		return Vector2.RIGHT
+		
+	elif Input.is_action_just_pressed("move_left"):
+		return Vector2.LEFT
+		
+	elif Input.is_action_just_pressed("move_down"):
+		return Vector2.DOWN   # Fixed: Changed 'if' to 'return'
+		
+	elif Input.is_action_just_pressed("move_up"):
+		return Vector2.UP     # Fixed: Changed 'if' to 'return'
+		
+	return Vector2.ZERO
