287e6115c553d3f8d2ec5e7f1bbb55c7c0dbaca6
Jack <jack@Davids-Mac-mini.local>
2026-09-24 13:25:37 -0700

    save

--- scripts/player2d.gd
+++ scripts/player2d.gd
@@ -5,7 +5,9 @@ const tile_size = 10
 var direction = Vector2.ZERO
 var speed = 30.0
 var attack_power = 1
+var anim_direction = Vector2.RIGHT
 
+@onready var anim = $AnimatedSprite2D
 @onready var raycast = $RayCast2D
 
 func _ready() -> void:
@@ -14,24 +16,31 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	direction = get_input()
 	
+	play_anim()
+	
 	var origin = global_position
 	var target = global_position + (direction * tile_size)
 	
+	raycast.target_position = direction * tile_size * 0.3
+	raycast.force_raycast_update()
+	
 	if raycast.is_colliding():
 		var collider = raycast.get_collider()
-		var hit_point = raycast.get_collision_point()
-		var tile_data = collider.get_cell_tile_data(hit_point + (direction * 0.1))
 		
-		if tile_data != null:
-			var tile_number = tile_data.get_custom_data("block_type")
-				
-			if tile_number == 1:
-				print("you hit a wall!")
-				return
+		if collider != null and collider.has_method("get_cell_tile_data"):
+			var hit_point = raycast.get_collision_point()
+			var map_position = collider.local_to_map(hit_point + direction * 2)
+			var tile_data = collider.get_cell_tile_data(map_position)
 			
-		elif collider.is_in_group("enemy"):
+			if tile_data != null:
+				var tile_number = tile_data.get_custom_data("block_type")
+				if tile_number == 1:
+					print("you hit a wall!")
+					return
+					
+		elif collider != null and collider.is_in_group("enemy"):
 			pass
-	
+
 	if direction != Vector2.ZERO:
 		global_position += direction * tile_size
 
@@ -50,6 +59,23 @@ func get_input():
 		
 	return Vector2.ZERO
 
+func play_anim():
+		
+	if direction == Vector2.RIGHT:
+		anim_direction = Vector2.RIGHT
+	elif direction == Vector2.LEFT:
+		anim_direction = Vector2.LEFT
+	
+	if anim_direction == Vector2.RIGHT:
+		anim.flip_h = false
+	else:
+		anim.flip_h = true
+		
+	if direction * speed != Vector2(0, 0):
+		anim.animation = "run"
+	else:
+		anim.animation = "idle"
+		
 func apply_damage(target: CharacterBody2D, damage: int):
 	if target == null:
 		return
